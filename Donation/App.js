68b8631b4ec3d23dc83733d399b70414843cef03
Hossam Emam <78453559+hossamemamo@users.noreply.github.com>
2023-05-08 19:45:49 +0300

    Delete App.js

--- Donation/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-//React navigation stack
-//import RootStack from './navigators/RootStack';
-import Blood from './screens/Blood';
-export default function App() {
-  return <Blood/>;
-}
